48500edadd6454ecdb0225f5fe6a1c785452c834
edgarbm16 <2c.edgar.barron@gmail.com>
2022-11-05 22:28:19 -0600

    Login y validacion

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AlertController, LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController, NavController, ToastController} from '@ionic/angular';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -9,31 +10,69 @@ import { AlertController, LoadingController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
+  Contenido: any;
+  mail = [];
+  pass = [];
+
+  email:string;
+  password:string;
+
   private EMAILPATTERN = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
   constructor(
     private alertCtrl: AlertController,
     private loadingCtrl: LoadingController,
-  ) { }
+    private toastController: ToastController,
+    private auth: AuthServiceService,
+    private NavigatorCtrl: NavController
+  ) {
+    this.auth.authService().subscribe((response)=>{
+
+      this.Contenido = response['results']; 
+      
+      for (let index = 0; index < this.Contenido.length; index++) {
+        this.mail.push(
+          this.Contenido[index].email,
+        );
+
+        this.pass.push(
+          this.Contenido[index].password
+        );
+      }     
+      // console.log(this.data);
+
+    }).unsubscribe;
+   }
 
   loginForm = new FormGroup({
     email: new FormControl('',[Validators.required, Validators.pattern(this.EMAILPATTERN)]),
-    password: new FormControl('',[Validators.required, Validators.minLength(10)]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8)]),
   });
 
   ngOnInit() {
     this.loginForm.controls;
   }
 
-  onSubmit(){
+  onSubmit(){    
     if(this.loginForm.value.email === undefined || this.loginForm.value.email === null){
       this.validateAlert('Error Datos', 'Ingrese un correo');
     } else if (this.loginForm.value.password === undefined || this.loginForm.value.password === null){
       this.validateAlert('Error Datos', 'Inglese una contraseña');
+    } else if(this.mail.includes(this.email) && this.pass.includes(this.password)) {
+      this.NavigatorCtrl.navigateRoot('/home');
     } else {
-            
+      this.validateAlert('Datos Incorrectos', 'La contraseña o el correo no son correctos :(');
     }
   }
 
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: '¡Bienvenido a Cook Home!',
+      duration: 2500,
+      position: position
+    });
+
+    await toast.present();
+  }
 
   async validateAlert(title, message){
     const alert = await this.alertCtrl.create({
--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
+import { environment as ENV } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+
+export interface AuthLogin {
+  objectId?: string;
+  username?: string;
+  email?: string;
+  telefono?: string;
+  password?: string;
+  updateAt?: string;
+  createdAt?: string;
+  ACL?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
+
+  private url = ENV.API + 'classes/users';
+  private headerGet = {
+    "X-Parse-Application-Id": ENV.APP_ID,
+    "X-Parse-REST-API-Key": ENV.API_SECRET,
+  }
 
   constructor(
     private http: HttpClient
@@ -12,7 +30,7 @@ export class AuthServiceService {
 
 
   authService(){
-    return 
+    return this.http.get<[AuthLogin]>(this.url, {headers: this.headerGet});
   }
 }
 
