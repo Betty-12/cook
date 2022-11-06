65a05620395cb1e4d247bc930e52adb522b6e22d
Betty-12 <beatriz_365@outlook.es>
2022-11-06 11:45:25 -0600

    correccionesdos

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 import { ProductAddService } from 'src/app/service/product-add.service';
 
 @Component({
@@ -12,15 +15,31 @@ export class HomePage {
   Contenido: any;
   constructor(
     private productos: ProductAddService,
+    private storage: Storage,
+    private navCtrl: NavController,
+    private router: Router,
   ) {
-    this.productos.getProducts().subscribe((response)=>{
+    this.productos.getProducts().subscribe((response) => {
       console.log("DATA", response);
-      
+
       this.Contenido = response['results'];
     }).unsubscribe;
 
   }
 
+  onClick(producto) {
+    const idProduct = producto.objectId;
+    this.storage.set('producto ' + idProduct, producto).then((resp) => {
+      console.log("Producto::", resp);
+      const queryParams = {
+        objectId: resp.objectId,
+        nombre: resp.nombre,
+        precio: resp.precio,
+      }
+      this.router.navigate(['/add-car'], { queryParams })
+    });
 
 
+
+  }
 }
