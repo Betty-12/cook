3f2891b834b8eec2a8cfbea9a4f44d73faac9652
edgarbm16 <2c.edgar.barron@gmail.com>
2022-11-05 20:00:16 -0600

    Diseños de login y register

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { ProductAddService } from 'src/app/service/product-add.service';
-import { Storage } from '@ionic/storage-angular';
-import { NavController } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,31 +10,14 @@ export class HomePage {
 
 
   Contenido: any;
-
-  ProductoAdd: any;
   constructor(
     private productos: ProductAddService,
-    private storage: Storage,
-    private navCtrl: NavController,
-    private router: Router,
   ) {
-    this.productos.getProducts().subscribe((response) => {
+    this.productos.getProducts().subscribe((response)=>{
+      console.log("DATA", response);
+      
       this.Contenido = response['results'];
     }).unsubscribe;
-  }
-
-
-  onClick(producto) {
-    const idProduct = producto.objectId;
-    this.storage.set('producto ' + idProduct, producto).then((resp) => {
-      console.log("Producto::", resp);
-      const queryParams = {
-        objectId: resp.objectId,
-        nombre: resp.nombre,
-        precio: resp.precio,
-      }
-      this.router.navigate(['/add-car'], {queryParams})
-    });
 
   }
 
